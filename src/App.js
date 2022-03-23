a59e007082be279a258ed883fa183e2e8435fefc
Behzat Bilgin Erdem <e200456@metu.edu.tr>
2022-03-23 03:49:49 +0300

    formik email&username

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { useFormik, Field, Form } from "formik";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const formik = useFormik({
+		initialValues: {
+			firstName: "",
+			lastName: "",
+			email: "",
+		},
+		onSubmit: (values) => {
+			alert(JSON.stringify(values, null, 2));
+		},
+	});
+	return (
+		<form onSubmit={formik.handleSubmit}>
+			<label htmlFor="firstName">First Name</label>
+			<input
+				id="firstName"
+				name="firstName"
+				type="text"
+				onChange={formik.handleChange}
+				value={formik.values.firstName}
+			/>
+
+			<label htmlFor="lastName">Last Name</label>
+			<input
+				id="lastName"
+				name="lastName"
+				type="email"
+				onChange={formik.handleChange}
+				value={formik.values.lastName}
+			/>
+
+			<label htmlFor="email">Email</label>
+			<input
+				id="email"
+				name="email"
+				type="email"
+				onChange={formik.handleChange}
+				value={formik.values.email}
+			/>
+
+			<button type="submit">Submit</button>
+		</form>
+	);
 }
 
 export default App;
